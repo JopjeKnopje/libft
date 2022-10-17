8bf1f3e1af2ab5ab05bf469784b83f0ffc32385e
Joppe Boeve <jboeve@f0r5s18.codam.nl>
2022-10-17 11:28:45 +0200

    forgot to add ft_striteri.c

---
+++ src/ft_striteri.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_striteri.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jboeve <jboeve@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/17 11:09:03 by jboeve        #+#    #+#                 */
+/*   Updated: 2022/10/17 11:27:17 by jboeve        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void 	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int i;	
+
+	i = 0;
+	while (s[i])
+	{
+		f((unsigned int) i, &s[i]);
+		i++;
+	}
+}
