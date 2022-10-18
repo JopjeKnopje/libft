88b844602a93398534090b77e56b0e4817f0f8d8
Joppe Boeve <jboeve@f0r5s19.codam.nl>
2022-10-18 13:33:14 +0200

    did strncmp

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <jboeve@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/10 13:43:39 by jboeve        #+#    #+#                 */
-/*   Updated: 2022/10/10 15:27:07 by jboeve        ########   odam.nl         */
+/*   Updated: 2022/10/18 13:32:32 by jboeve        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,13 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t  i;
-	size_t  j;
-
+	
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	while ((s1[i] && s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
 			break;
 		i++;
-		j = 0;
-		while (s1[i] == s2[i + j])
-			j++;
 	}
 	return (s1[i] - s2[i]);
 }
