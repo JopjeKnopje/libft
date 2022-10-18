dfdfe514cf4ea3d48195b4b1deffdf197df9b44a
Joppe Boeve <jboeve@f0r5s19.codam.nl>
2022-10-18 11:58:48 +0200

    did strlcat

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <jboeve@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/06 11:49:21 by jboeve        #+#    #+#                 */
-/*   Updated: 2022/10/17 16:32:31 by jboeve        ########   odam.nl         */
+/*   Updated: 2022/10/18 11:58:38 by jboeve        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,20 @@
 int ft_strlcat(char *dest, const char *src, size_t size)
 {
 
-	return (0);
+	size_t offset;
+	size_t i;
+	
+	offset = ft_strlen(dest);
+	if (size <= offset)
+		return (size + ft_strlen(src));
+
+	i = 0;
+	while (src[i] && i + offset < size - 1)
+	{
+		dest[offset + i] = src[i];
+		i++;
+	}
+	dest[offset + i] = 0;
+
+	return (offset + ft_strlen(src));
 }
