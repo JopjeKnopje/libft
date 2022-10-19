62aaca1afb676d68cf37bd29ed9b9803a39d66b2
Joppe Boeve <jboeve@f0r4s20.codam.nl>
2022-10-19 13:15:34 +0200

    worked on split

--- src/ft_split.c
+++ src/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <jboeve@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/13 11:47:26 by jboeve        #+#    #+#                 */
-/*   Updated: 2022/10/17 10:04:12 by joppe         ########   odam.nl         */
+/*   Updated: 2022/10/19 13:14:42 by jboeve        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,16 @@
 
 char	**ft_split(char const *s, char c)
 {
-	char *post = ft_strchr(s, (int) c);
+	// Todo make modular
+
+	char *delim = ft_strchr(s, c);
+	int delim_index = (delim - s);
+	char *pre = ft_substr(s, 0, delim_index);
+	char *post = ft_substr(s, delim_index + 1, ft_strlen(delim + 1));
 	
-	size_t post_len = ft_strlen(post);
-	size_t pre_len = ft_strlen(s) - post_len;
-
-	char *pre = malloc(pre_len * sizeof(char) + 1);
-
-	ft_strlcpy(pre, s, pre_len + 1);
-
-	// TODO Should be sizeof(char)
-	char **arr = malloc(sizeof (char*) * 2);
-	arr[0]  = malloc(sizeof (char*) * pre_len + 1);
-	arr[0] = pre;
-	arr[1] = (post + 1);
-
-	return arr;
-
+	char **s_split = (char **) malloc((sizeof(char **) * 2) + 1);
+	s_split[0] = pre;
+	s_split[1] = post;
+	s_split[2] = 0;
+	return s_split;
 }
