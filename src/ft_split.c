3fe891e4559eba2b41c1cb2c77d049c2eb8bfa43
Joppe Boeve <jboeve@f0r5s18.codam.nl>
2022-10-20 12:46:31 +0200

    crying

--- src/ft_split.c
+++ src/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <jboeve@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/13 11:47:26 by jboeve        #+#    #+#                 */
-/*   Updated: 2022/10/20 09:34:48 by joppe         ########   odam.nl         */
+/*   Updated: 2022/10/20 12:46:00 by jboeve        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,53 @@
 // count words
 // malloc string array
 // substr the words
+
+int	count_words(char const *s, char c)
+{
+	char	*delim;
+	int		word_count;
+
+	delim = (char *) s;
+	word_count = 0;
+	while (!word_count || (delim = ft_strchr(delim + 1, c)))
+	{
+		delim++;
+		if (!(ft_strchr(delim + 1, c)))
+			break ;
+		if (delim[0] == c)
+			continue ;
+		word_count++;
+	}
+	return word_count;
+}
+
+
 char	**ft_split(char const *s, char c)
 {
-	char *delim = (char *) s;
-	int len = 0;
-	char *s_sub = 0;
-	int i = 0;
+	int i;
+	char *delim;
+	int word_count;
+	char **split;
 
+	i = 0;
+	word_count = count_words(s, c);
+	split = (char **) ft_calloc((word_count + 1), sizeof(char **));
+
+	delim = (char *) s;
 	while (!i || (delim = ft_strchr(delim + 1, c)))
 	{
-		if (!(len = ft_strchr(delim, c) - delim))
-			len = ft_strlen(delim);
-		s_sub = ft_substr(delim, 0, len);
-		printf("delim %s\n", delim);
-		printf("sub %s\n", s_sub);
+		delim++;
+		if (!(ft_strchr(delim + 1, c)))
+			break ;
+		if (delim[0] == c)
+			continue ;
+		
+		printf("%s\n", delim);
 		i++;
 	}
 
-		
+	
+	free(split);
+
 	return NULL;
 }
