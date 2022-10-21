92025bc9e8bbe54ccb433644776d44078bda5518
JopjeKnopje <jboeve009@gmail.com>
2022-10-21 16:23:50 +0200

    changed substr.c

--- src/ft_substr.c
+++ src/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <jboeve@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 11:38:15 by jboeve        #+#    #+#                 */
-/*   Updated: 2022/10/20 10:31:48 by joppe         ########   odam.nl         */
+/*   Updated: 2022/10/21 16:23:00 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,19 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;		
-	char	*s_d;
+	char	*s_sub;
 
-	if (!len)
-		return (ft_strdup(""));
-	s_d = malloc((len * sizeof(char)) + 1);
-	if (!s_d)
+	if (!s)
+		return (NULL);
+	s_sub = malloc(sizeof(char) * (len + 1));
+	if (!s_sub)
 		return (NULL);
 	i = 0;
 	while (s[i + start] && i < len)
 	{
-		s_d[i] = s[i + start];
+		s_sub[i] = s[i + start];
 		i++;
 	}
-	s_d[i] = 0;
-	return (s_d);
+	s_sub[i] = 0;
+	return (s_sub);
 }
