ee3426cdb3f84bfa9d246b1af4a21a0d9e20d7e3
JopjeKnopje <jboeve009@gmail.com>
2022-10-21 09:56:06 +0200

    word count works

--- src/ft_split.c
+++ src/ft_split.c
@@ -6,31 +6,55 @@
 /*   By: jboeve <jboeve@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/13 11:47:26 by jboeve        #+#    #+#                 */
-/*   Updated: 2022/10/20 09:34:48 by joppe         ########   odam.nl         */
+/*   Updated: 2022/10/21 09:55:24 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
+#include <stdlib.h>
 
+int	count_words(char const *s, char c)
+{
+	int i;
+	int at_word;
+	int count;
+
+	count = 0;
+	i = 0;
+	at_word = 0;
+	while (s[i])
+	{
+		if (s[i] != c && !at_word)
+		{
+			at_word = 1;
+			count++;
+		}
+		else if (s[i] == c)
+			at_word = 0;
+		i++;
+	}
+	printf("%d\n", count);
+	return count;
+
+}
 // count words
 // malloc string array
 // substr the words
 char	**ft_split(char const *s, char c)
 {
-	char *delim = (char *) s;
-	int len = 0;
-	char *s_sub = 0;
-	int i = 0;
+	int word_count = count_words(s, c);
+	int i;
 
-	while (!i || (delim = ft_strchr(delim + 1, c)))
+	char **split = (char **) malloc((word_count + 1) * sizeof(char **));
+	if (split)
+		return (NULL);
+
+	i = 0;
+	while (i <= word_count)
 	{
-		if (!(len = ft_strchr(delim, c) - delim))
-			len = ft_strlen(delim);
-		s_sub = ft_substr(delim, 0, len);
-		printf("delim %s\n", delim);
-		printf("sub %s\n", s_sub);
-		i++;
+		
+
 	}
 
 		
