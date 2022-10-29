05eaecb72399470fdff7f70af7dcc5d45dfa7584
joppe <jboeve009@gmail.com>
2022-10-29 23:46:01 +0200

    fixed

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <jboeve@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/10 13:43:39 by jboeve        #+#    #+#                 */
-/*   Updated: 2022/10/20 10:28:10 by joppe         ########   odam.nl         */
+/*   Updated: 2022/10/29 23:45:10 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
-	while ((s1[i] && s2[i]) && i < n)
-	{
-		if (s1[i] != s2[i])
-			break ;
+	while (i < n && s1[i] && (s1[i] == s2[i]))
 		i++;
-	}
-	return (s1[i] - s2[i]);
+	if (i == n)
+		return (0);
+	return ((unsigned char) s1[i] - (unsigned char) s2[i]);
 }
