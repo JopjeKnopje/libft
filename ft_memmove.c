dfbf58837d1731e942924c1a6e78cd0580f0aa85
joppe <jboeve009@gmail.com>
2022-10-29 20:09:44 +0200

    fixed ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: joppe <marvin@42.fr>                         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/05 15:04:57 by joppe         #+#    #+#                 */
-/*   Updated: 2022/10/24 16:35:14 by jboeve        ########   odam.nl         */
+/*   Updated: 2022/10/29 20:09:15 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,25 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	int		i;
+	size_t	i;
 	char	*s_ptr;
 	char	*d_ptr;
-	char	tmp[n];
 
-	if (!n || !dest || !src)
+	if (!dest && !src)
 		return (dest);
-
-	printf("test123\n");
-
 	s_ptr = (char *) src;
 	d_ptr = (char *) dest;
 	i = 0;
-	while (s_ptr[i])
+	if (d_ptr > s_ptr)
+		while (n--)
+			d_ptr[n] = s_ptr[n];
+	else
 	{
-		tmp[i] = s_ptr[i];
-		i++;
-	}
-	i = 0;
-	while (tmp[i])
-	{
-		d_ptr[i] = tmp[i];
-		i++;
+		while (i < n)
+		{
+			d_ptr[i] = s_ptr[i];
+			i++;
+		}
 	}
 	return (d_ptr);
 }
