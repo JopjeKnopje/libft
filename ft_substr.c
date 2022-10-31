a5881f3f0757639765ff838a44f05b982a23916e
JopjeKnopje <jboeve009@gmail.com>
2022-10-31 17:28:57 +0100

    worked more on substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jboeve <jboeve@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 11:38:15 by jboeve        #+#    #+#                 */
-/*   Updated: 2022/10/31 16:18:12 by jboeve        ########   odam.nl         */
+/*   Updated: 2022/10/31 17:28:15 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,15 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t i;
 	char *s_sub;
-	
-	if (!s || !(len + 1) || start >= (unsigned int) ft_strlen(s))
+
+	if (!s)
 		return (ft_strdup(""));
-	if (start + len >= (unsigned int) ft_strlen(s))
-		len = len - start;
-	// hello
-	// start: 1
-	// len: 5
-	// len = len - start
 	s_sub = (char *) malloc(sizeof(char) * (len + 1));
 	if(!s_sub)
 		return (NULL);
 	i = 0;
-	while (s[i] && i < len)
+	while (s[start + i] && i < len)
 	{
-		// printf("char %c\n", s[start + i]);
 		s_sub[i] = s[start + i];
 		i++;
 	}
