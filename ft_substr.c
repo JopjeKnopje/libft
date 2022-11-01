1f6ee926038aa72e9cc56a58a588b75bd96143bb
joppe <jboeve009@gmail.com>
2022-11-01 18:53:34 +0100

    substr works

--- ft_substr.c
+++ ft_substr.c
@@ -6,23 +6,31 @@
 /*   By: jboeve <jboeve@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 11:38:15 by jboeve        #+#    #+#                 */
-/*   Updated: 2022/11/01 17:39:09 by joppe         ########   odam.nl         */
+/*   Updated: 2022/11/01 18:52:51 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	i;
+	size_t	s_len;
 	char	*s_sub;
 
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start > s_len)
+		return (ft_strdup(""));
+	s_len -= start;
+	if (len > s_len)
+		len = s_len;
 	s_sub = malloc(sizeof(char) * (len + 1));
 	if (!s_sub)
 		return (NULL);
 	i = 0;
-	while (s[start + i] && i < len)
+	while (i < len)
 	{
 		s_sub[i] = s[start + i];
 		i++;
