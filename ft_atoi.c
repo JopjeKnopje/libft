d0febfcd6d19ddeef677197ad7a5db0addd6cbc7
JopjeKnopje <jboeve009@gmail.com>
2022-11-01 17:34:35 +0100

    more ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,53 +6,33 @@
 /*   By: joppe <jboeve@student.codam.nl>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 09:41:09 by joppe         #+#    #+#                 */
-/*   Updated: 2022/11/01 08:20:13 by joppe         ########   odam.nl         */
+/*   Updated: 2022/11/01 16:27:40 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
 
 int	ft_atoi(const char *s)
 {
-	int	num;
 	int	i;
+	int	num;
 	int	sign;
 
-	i = 0;
 	num = 0;
 	sign = 0;
-	while (s[i])
+	i = 0;
+	while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13))
+		i++;
+	while ((s[i] == '-' || s[i] == '+') && !sign) 
 	{
-		// printf("%d @ %c | %d\n", i, s[i], s[i]);
-		if (s[i] == ' ')
-		{
-			i++;
-			continue;
-		}
-		if (s[i] >= 7 && s[i] <= 13)
-		{
-			if (s[i + 1] == ' ' && ft_isdigit(s[i + 2])) 
-			{
-				return (0);
-			}
-			i++;
-			continue;
-		}
-		break;
-	}
-	if (s[i] == '-' && sign == 0)
-	{
-		sign = -1;
+		if (s[i] == '-')
+			sign = -1;
+		if (s[i] == '+')
+			sign = 1;
 		i++;
 	}
-	if (s[i] == '+' && sign == 0)
-	{
-		sign = 1;
-		i++;
-	}
-	if (sign == 0)
-		sign = 1;
+	// sign = (sign == 0);
 	while (s[i])
 	{
 		if (!ft_isdigit(s[i]))
