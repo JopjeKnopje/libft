cd7d02680f1deeda2f3d5b3b0bf6f77449269a7e
joppe <jboeve009@gmail.com>
2022-11-01 19:16:32 +0100

    ft_strtrim done

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: joppe <jboeve@student.codam.nl>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 20:12:33 by joppe         #+#    #+#                 */
-/*   Updated: 2022/11/01 07:37:24 by joppe         ########   odam.nl         */
+/*   Updated: 2022/11/01 19:14:59 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,5 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	start = find_start(s1, set);
 	end = find_end(s1, set);
-	printf("start %d | end %d\n", start, end);
 	return (ft_substr(s1, start, end - start + 1));
 }
