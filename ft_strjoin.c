54e049541d97e39e5d4b46c307efef20b3bdb221
joppe <jboeve009@gmail.com>
2022-11-02 21:36:45 +0100

    working on itoa

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,11 +6,12 @@
 /*   By: joppe <jboeve@student.codam.nl>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/12 19:59:09 by joppe         #+#    #+#                 */
-/*   Updated: 2022/10/20 10:24:31 by joppe         ########   odam.nl         */
+/*   Updated: 2022/11/02 19:14:27 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -18,14 +19,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*offset;
 	size_t	total_size;
 
-	total_size = ft_strlen(s1) + ft_strlen(s2);
-	buf = (char *) malloc(total_size + 1);
+	total_size = ft_strlen(s1) + ft_strlen(s2) + 1;
+	buf = (char *) malloc(total_size);
 	if (!buf)
 		return (NULL);
 	offset = (buf + ft_strlen(s1));
 	if (!ft_memcpy(buf, s1, ft_strlen(s1)))
 		return (NULL);
+	// printf("s1 memcpy %s\n", );
 	if (!ft_memcpy(offset, s2, ft_strlen(s2)))
 		return (NULL);
+	// printf("s2 memcpy %s\n", (buf + ft_strlen(s2)));
+	// printf("total %s\n", buf);
+	buf[total_size] = 0;
 	return (buf);
 }
