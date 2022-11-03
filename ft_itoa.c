51eec804ad14946b24a73a891d73295a038c1a2f
JopjeKnopje <jboeve009@gmail.com>
2022-11-03 09:20:57 +0100

    very nice

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: joppe <jboeve@student.codam.nl>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/17 00:22:38 by joppe         #+#    #+#                 */
-/*   Updated: 2022/11/02 21:33:23 by joppe         ########   odam.nl         */
+/*   Updated: 2022/11/03 09:20:17 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,28 +28,27 @@ int	int_len(int n)
 char	*ft_itoa(int n)
 {
 	int		i;
-	int		len;
 	int		sign;
 	int		value;
 	char	*s;
 
+	if (!n)
+		return (ft_strdup("0"));
 	sign = (n < 0);
-	len = int_len(n) + sign;
-	s = (char *) malloc(sizeof(char) * (len + 1));
+	i = int_len(n) + sign;
+	s = (char *) malloc(sizeof(char) * (i + 1));
 	if (!s)
 		return (NULL);
 	if (sign)
 		s[0] = '-';
-	i = len - 1;
+	s[i-- + 1] = 0;
 	while (n)
 	{
 		value = (n % 10);
 		if (sign)
 			value *= -1;
-		s[i] = value + '0';
+		s[i--] = value + '0';
 		n /= 10;
-		i--;
 	}
-	s[len + 1] = 0;
 	return (s);
 }
