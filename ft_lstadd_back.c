39032c44cb337d4bcb83011b5eaef8bec7daa424
joppe <jboeve009@gmail.com>
2022-11-03 23:46:44 +0100

    lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: joppe <jboeve@student.codam.nl>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/11/03 18:17:05 by joppe         #+#    #+#                 */
-/*   Updated: 2022/11/03 18:18:09 by joppe         ########   odam.nl         */
+/*   Updated: 2022/11/03 23:43:39 by joppe         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,5 @@
 
 void ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list *tmp;
-
-	tmp = ft_lstlast(lst[0]);
-	tmp->next = new;
+	ft_lstlast(*lst)->next = new;
 }
